e8cc69e94c112d25c87016a7520a321fe0d2aa7b
Carvalho <braga.gabrielcarvalho@gmail.com>
2025-02-06 19:20:23 -0300

    add: created PolicySerializer for Policy model

---
+++ insuranceManager/api/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Policy
+
+class PolicySerializer(serializers.ModelSerializer):
+    customer_name = serializers.CharField(source='customer.name')
+    policy_id = serializers.UUIDField(source='id', read_only=True)
+    policy_type = serializers.CharField(source='type')
+    expiry_date = serializers.DateTimeField(source='expiry_date')
+    
+    class Meta:
+        model = Policy
+        fields = ['policy_id', 'customer_name', 'policy_type', 'expiry_date']
