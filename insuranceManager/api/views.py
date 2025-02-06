c6d74b5885aaf69030ef5a69b152684d20e9fe79
Carvalho <braga.gabrielcarvalho@gmail.com>
2025-02-06 19:33:15 -0300

    feat: Add POST method for policy management

--- insuranceManager/api/views.py
+++ insuranceManager/api/views.py
@@ -8,10 +8,17 @@ from .serializer import PolicySerializer
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def policy_list(request):
     if request.method == 'GET':
         queryset = Policy.objects.all()
         serializer = PolicySerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = PolicySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         return Response(serializer_class.data, status=status.HTTP_200_OK)
